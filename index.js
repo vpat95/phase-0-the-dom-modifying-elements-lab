1b4ad1242226481573556f41ae2efc00d44fcb4a
Vivek Patel <vpatel360@gmail.com>
2022-08-05 13:56:03 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+//Remove main
+
+main.remove()
+
+// Add h1 with id = "victory"
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+// Add content + attach node to DOM
+
+newHeader.textContent = "Vivek is the champion";
