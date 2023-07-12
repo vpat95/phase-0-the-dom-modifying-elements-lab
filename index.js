c97dcb3e9609fec346617cbe142aefdc3c22f27e
Vivek Patel <vpatel360@gmail.com>
2023-07-12 13:24:56 -0400

    commit tree 8ad46d0cc7b139b03e6cc4194bbaf43514dc66e9

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ main.remove()
 
 // Add h1 with id = "victory"
 
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
 
 // Add content + attach node to DOM
 
-newHeader.textContent = "Vivek is the champion";
+newHeader.textContent = `Vivek Patel is the champion`
+document.body.append(newHeader)
